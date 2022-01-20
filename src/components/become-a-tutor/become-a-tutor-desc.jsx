69600a5936b15139f4b82098526e69012db0fc9b
Daniel Hong <dan8492@gmail.com>
2022-01-19 22:08:47 -0800

    entered in the description for the become a tutor page

--- src/components/become-a-tutor/become-a-tutor-desc.jsx
+++ src/components/become-a-tutor/become-a-tutor-desc.jsx
@@ -2,28 +2,43 @@ const BecomeATutorDescription = () => (
   <div className="flex center justify-center">
     <div>
       <p>
-
-    Q&A Tutoring is always searching for capable, personable, and qualified tutors.  We believe our tutors are the most valuable assets to our business. If you believe you possess qualities that make you a desirable tutor we encourage you to apply!
-
-That being said, Q&A is very meticulous about choosing the people we work with. We aim to provide the highest possible standards of tutoring available to customers. To be considered as a candidate for Q&A Tutoring you must demonstrate with tangible proof your abilities to be an expert in your subject matter as well as showcase your ability to make students feel comfortable through your approachable and friendly personality. You will go through an examination process as well as two separate interviews to determine if you are a qualified prospect. We also require transcripts that show a high level of success in academics in order to solidify your proven expertise in the subject matter you are applying for.
-
-We are well aware of the extremely high value that a good, qualified tutor brings to the table. So to support such high standards we back our requirements up with extremely competitive compensation. We also realize that tutors of such standards are likely pursuing higher education or a career so in order to accommodate our tutors also set their own hours and have the right to a flexible schedule
-
-We look forward to hearing from you!
+        Q&A Tutoring is always searching for capable, personable, and qualified tutors. We
+        believe our tutors are the most valuable assets to our business. If you believe you
+        possess qualities that make you a desirable tutor we encourage you to apply!
+      </p>
+      <p>
+        That being said, Q&A is very meticulous about choosing the people we work with. We
+        aim to provide the highest possible standards of tutoring available to customers. To
+        be considered as a candidate for Q&A Tutoring you must demonstrate with tangible
+        proof your abilities to be an expert in your subject matter as well as showcase your
+        ability to make students feel comfortable through your approachable and friendly
+        personality. You will go through an examination process as well as two separate
+        interviews to determine if you are a qualified prospect. We also require transcripts
+        that show a high level of success in academics in order to solidify your proven
+        expertise in the subject matter you are applying for.
+      </p>
+      <p>
+        We are well aware of the extremely high value that a good, qualified tutor brings to
+        the table. So to support such high standards we back our requirements up with
+        extremely competitive compensation. We also realize that tutors of such standards
+        are likely pursuing higher education or a career so in order to accommodate our
+        tutors also set their own hours and have the right to a flexible schedule
+      </p>
+      <p>
+        We look forward to hearing from you!
       </p>
     </div>
     <div>
-      <p>
-
-    Extremely thorough knowledge of subject matter
-Minimum GPA of 3.5 or higher
-Must have received A’s in classes that relate to subject matter
-Ability to effectively communicate subject matter
-Application of different learning strategies
-Ability to listen and answer questions during tutoring sessions
-Set goals and expectations during the first few sessions
-Ability to be dynamic in tutoring approach, based on the student’s learning style.
-      </p>
+      <ul>
+        <li>Extremely thorough knowledge of subject matter</li>
+        <li>Minimum GPA of 3.5 or higher</li>
+        <li>Must have received A&apos;s in classes that relate to subject matter</li>
+        <li>Ability to effectively communicate subject matter</li>
+        <li>Application of different learning strategies</li>
+        <li>Ability to listen and answer questions during tutoring sessions</li>
+        <li>Set goals and expectations during the first few sessions</li>
+        <li>Ability to be dynamic in tutoring approach, based on the student&apos;s learning style.</li>
+      </ul>
     </div>
   </div>
 )
