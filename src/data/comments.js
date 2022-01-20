bc91589f76e7d041ce52a374b447f2d68bfd42f5
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-01-19 20:18:00 -0800

    Added other necessities

---
+++ src/data/comments.js
@@ -0,0 +1,24 @@
+const testimonials = [{
+  title: `"Best Tutoring Ever"`,
+  content: `My dream has come true! I was able to start working on getting my
+              degree, and if it wasn't or your help, it would have taken me quite some
+              time to reach the goal. Thank you very much, and please, continue your great job!`,
+  name: `Pamela R.`,
+  occupation: `Student`
+}, {
+  title: `"Great Tutor!"`,
+  content: `Quazi worked on AP Biology with my son. He is a walking encyclopedia. Quazi
+              gave my son a lot of tips on how to prepare not only for exams but everyday.
+              He was polite and punctual`,
+  name: `Carol A.`,
+  occupation: `Parent`
+}, {
+  title: `"Timely Help"`,
+  content: `I was provided responsive, timely assistance to my daughter in preparing for
+              AP Calculus exam. Sarah was punctual, prepared, and knowledgeable. We are
+              very appreciative!`,
+  name: `Aaron J.`,
+  occupation: `Parent`
+}]
+
+export default testimonials
